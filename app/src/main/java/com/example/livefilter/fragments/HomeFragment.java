ce85e0f6d5499d0bce9137a7668ba08d104a13a9
Alana White <alanaw@fb.com>
2020-07-16 18:06:22 -0400

    created class to model filters for sharing

--- app/src/main/java/com/example/livefilter/fragments/HomeFragment.java
+++ app/src/main/java/com/example/livefilter/fragments/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,8 @@ import com.example.livefilter.R;
 
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView rvFilters;
 
 
     public HomeFragment() {
@@ -31,5 +34,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        rvFilters = view.findViewById(R.id.rvFilters);
     }
 }
---
+++ app/src/main/java/com/example/livefilter/models/FilterPost.java
@@ -0,0 +1,58 @@
+package com.example.livefilter.models;
+
+import com.parse.Parse;
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Filter")
+public class FilterPost extends ParseObject {
+
+    public static final String KEY_USER = "user";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_NAME = "filterName";
+    public static final String KEY_BEFORE = "beforeImage";
+    public static final String KEY_AFTER = "afterImage";
+
+    public String getName() {
+        return getString(KEY_NAME);
+    }
+
+    public void setName(String name) {
+        put(KEY_NAME, name);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseFile getBefore() {
+        return getParseFile(KEY_BEFORE);
+    }
+
+    public void setBefore(ParseFile parseFile) {
+        put(KEY_BEFORE, parseFile);
+    }
+
+    public ParseFile getAfter() {
+        return getParseFile(KEY_AFTER);
+    }
+
+    public void setAfter(ParseFile parseFile) {
+        put(KEY_AFTER, parseFile);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser parseUser) {
+        put(KEY_USER, parseUser);
+    }
+
+}

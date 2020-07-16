ac648fc5dc7702efff6684e44184f320f0bcea4c
Alana White <alanaw@fb.com>
2020-07-16 19:03:19 -0400

    configured adapter to display feed of filters in posts fragment

--- app/src/main/java/com/example/livefilter/adapters/FeedAdapter.java
+++ app/src/main/java/com/example/livefilter/adapters/FeedAdapter.java
@@ -1,33 +1,86 @@
 package com.example.livefilter.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.livefilter.R;
+import com.example.livefilter.models.FilterPost;
+import com.parse.ParseFile;
+
+import java.util.List;
+
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
+
+    private Context context;
+    private List<FilterPost> filters;
+
+    public FeedAdapter(Context context, List<FilterPost> filters) {
+        this.context = context;
+        this.filters = filters;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // inflate (create) a view for every possible item on the screen
+        View view = LayoutInflater.from(context).inflate(R.layout.item_filter, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // get post at position
+        FilterPost filterPost = filters.get(position);
+        // bind it to viewholder
+        holder.bind(filterPost);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return filters.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView tvName;
+        private TextView tvDescription;
+        private TextView tvUsername;
+        private ImageView ivBefore;
+        private ImageView ivAfter;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            // find views for view elements of filterPost
+            tvName = itemView.findViewById(R.id.tvName);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivBefore = itemView.findViewById(R.id.ivBefore);
+            ivAfter = itemView.findViewById(R.id.ivAfter);
+        }
+
+        public void bind(FilterPost filterPost) {
+            // bind post data to view elements
+            tvName.setText(filterPost.getName());
+            tvUsername.setText(filterPost.getUser().getUsername());
+            tvDescription.setText(filterPost.getDescription());
+            // load images using Glide
+            ParseFile beforeImage = filterPost.getBefore();
+            ParseFile afterImage = filterPost.getAfter();
+            if(beforeImage != null) {
+                Glide.with(context).load(beforeImage.getUrl()).into(ivBefore);
+            }
+            if(afterImage != null) {
+                Glide.with(context).load(afterImage.getUrl()).into(ivAfter);
+            }
         }
     }
 }
--- app/src/main/java/com/example/livefilter/fragments/HomeFragment.java
+++ app/src/main/java/com/example/livefilter/fragments/HomeFragment.java
@@ -5,18 +5,31 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.livefilter.R;
+import com.example.livefilter.adapters.FeedAdapter;
+import com.example.livefilter.models.FilterPost;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView rvFilters;
+    public static final String TAG = "HomeFragment";
+    protected RecyclerView rvFilters;
+    protected FeedAdapter adapter;
+    protected List<FilterPost> allPosts;
 
 
     public HomeFragment() {
@@ -36,5 +49,43 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         rvFilters = view.findViewById(R.id.rvFilters);
+
+        allPosts = new ArrayList<>();
+        adapter = new FeedAdapter(view.getContext(), allPosts);
+
+        // set adapter on RecyclerView
+        rvFilters.setAdapter(adapter);
+        // set layout manager on recyclerview
+        rvFilters.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        // get filters from parse
+        queryFilters();
+    }
+
+    private void queryFilters() {
+        // make a new query to get filters
+        ParseQuery<FilterPost> query = ParseQuery.getQuery(FilterPost.class);
+        // include user in information retrieved
+        query.include(FilterPost.KEY_USER);
+        // search for filter displays in background
+        query.findInBackground(new FindCallback<FilterPost>() {
+            @Override
+            public void done(List<FilterPost> filterPosts, ParseException e) {
+                // if unsuccessful, or error is thrown
+                if (e != null) {
+                    Log.e(TAG, "Issue with retrieving filters", e);
+                    return;
+                }
+                for(FilterPost post: filterPosts) {
+                    Log.i(TAG, "Filter: " + post.getName());
+                }
+
+                // save received posts, notify adapter of changes
+                allPosts.addAll(filterPosts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
     }
 }

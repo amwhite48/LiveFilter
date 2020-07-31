4861559c26a40fe74d8413e97583ab3c96e5f606
Alana White <alanaw@fb.com>
2020-07-31 18:38:10 -0400

    filter can be posted to Parse database and shows up on feed

--- app/src/main/java/com/example/livefilter/PostActivity.java
+++ app/src/main/java/com/example/livefilter/PostActivity.java
@@ -2,16 +2,31 @@ package com.example.livefilter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.livefilter.models.FilterPost;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.json.JSONArray;
 import org.parceler.Parcels;
 
+import java.io.File;
+
 public class PostActivity extends AppCompatActivity {
+
+    public static final String TAG = "PostActivity";
 
     EditText etFilterName;
     EditText etDescription;
@@ -24,12 +39,12 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post);
 
         // unwrap information passed into intent
-        String photoFile = getIntent().getExtras().getString("photoFilePath");
+        final String photoFile = getIntent().getExtras().getString("photoFilePath");
         String[] effectNames = getIntent().getExtras().getStringArray("effectNames");
         int[] effectIntensities = getIntent().getExtras().getIntArray("effectIntensities");
 
-        JSONArray effectNamesArray = convertStringArrayToJSON(effectNames);
-        JSONArray effectIntensitiesArray = convertIntArrayToJSON(effectIntensities);
+        final JSONArray effectNamesArray = convertStringArrayToJSON(effectNames);
+        final JSONArray effectIntensitiesArray = convertIntArrayToJSON(effectIntensities);
 
         // set up image views
         etFilterName = findViewById(R.id.etFilterName);
@@ -39,6 +54,67 @@ public class PostActivity extends AppCompatActivity {
 
         // load image from filepath into imageView
         ivFilteredImage.setImageBitmap(BitmapFactory.decodeFile(photoFile));
+
+        // when post button is clicked, save post to parse database
+        btPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get values from edit text fields
+                // make sure values passed in are not empty
+                String filterName = etFilterName.getText().toString();
+                if(filterName.isEmpty()) {
+                    Toast.makeText(PostActivity.this, "filter name cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String description = etDescription.getText().toString();
+                if(description.isEmpty()) {
+                    Toast.makeText(PostActivity.this, "description cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+                savePost(filterName, description, photoFile, effectNamesArray, effectIntensitiesArray, currentUser);
+            }
+        });
+
+
+    }
+
+    private void savePost(String filterName, String description, String photoFile, JSONArray effectNames, JSONArray effectIntensities, ParseUser currentUser) {
+
+        // create new object to store filterPost info
+        FilterPost filterPost = new FilterPost();
+
+        // set given parameters
+        filterPost.setName(filterName);
+        filterPost.setDescription(description);
+        filterPost.setEffectNames(effectNames);
+        filterPost.setEffectIntensities(effectIntensities);
+        filterPost.setUser(currentUser);
+
+        // set up new photoFile
+        File filteredPhoto = new File(photoFile);
+        filterPost.setAfter(new ParseFile(filteredPhoto));
+
+        // save callback to save post to parse in background
+        filterPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    // if an exception was thrown, there was an error
+                    Log.e(TAG, "Error while saving post", e);
+                    Toast.makeText(PostActivity.this, "Error saving post", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Log.i(TAG, "Posting successful!");
+
+                // launch main activity once post succeeded
+                goMainActivity();
+            }
+        });
 
 
     }
@@ -59,5 +135,12 @@ public class PostActivity extends AppCompatActivity {
             output.put(intArray[i]);
         }
         return output;
+    }
+
+    // launches main activity
+    private void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
